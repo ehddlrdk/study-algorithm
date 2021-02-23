e23f9d4875dd2e159bc85d3118494859af87fa24
kihong <sai223@ajou.ac.kr>
2021-02-23 23:47:48 +0900

    허허... 발전이 필요함...

---
+++ LeetCode/Search a 2D Matrix II 210223/honghong.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int col = matrix[0].size() - 1;
+        int row = 0;
+        
+        while(row < matrix.size() && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target) {
+                col--;
+            }
+            else
+                row++;
+        }
+        return false;
+    }
+};
